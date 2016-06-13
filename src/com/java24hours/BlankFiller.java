fd35f274532822859e5b6517e2229f298cbfd87c
matthew <matthew@AMD-8-CORE.home>
2016-06-13 02:10:49 -0700

    changed from singular to plural noun

--- src/com/java24hours/BlankFiller.java
+++ src/com/java24hours/BlankFiller.java
@@ -3,7 +3,7 @@ package com.java24hours;
 class BlankFiller{
 public static void main(String[] arguments){
     System.out.println("The " + arguments[0]
-        + " " + arguments[1] + " fox "
+        + " " + arguments[1] + " foxes "
         + "jumped over the "
         + arguments[2] + " dog. "
     );
