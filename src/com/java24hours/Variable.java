fb5e720b98b9c3fabd328ff366b1ae1993d2cab9
matthew <matthew@AMD-8-CORE.home>
2016-06-16 03:37:42 -0700

    added run instructions  to the BlankFiller.java file lines 2-11

---
+++ src/com/java24hours/Variable.java
@@ -0,0 +1,36 @@
+package com.java24hours;
+
+class Variable {
+    public static void main(String[] arguments){
+        int tops;
+        float gradePointAverage;
+        char key = 'C';
+        String productName ="Larvets";
+        byte escapeKey = 27;
+        short roomNumber = 222;
+        long salary = 264400000;
+        long salaryHumanReadable = 264_400_000;
+        boolean gameOver = false;
+        int mileage = 100;
+        int totalMileage = mileage;
+        
+        
+        float pi = 3.14F; //F indicates that the value is a float
+                          //D indicates that the value is a Double
+                          //L indicates that the value is a long 
+        
+        System.out.println(salaryHumanReadable);
+        System.out.println(gameOver);
+        System.out.println(totalMileage);
+        System.out.println(key);
+        System.out.println(pi);
+        System.out.println(escapeKey);
+        System.out.println(productName);
+        System.out.println(roomNumber);
+        System.out.println(salary);
+        
+        
+        
+    }
+    
+}
