07f072cac969d463c9afc03b7c2d948ebe1e77a2
matthew <matthew@AMD-8-CORE.home>
2016-06-18 03:51:54 -0700

    created a block of statements and code to printout the result of  whether or not one string is equal to another.

---
+++ src/com/java24hours/Favorite.java
@@ -0,0 +1,12 @@
+package com.java24hours;
+
+class Favorite{
+public static void main(String[] arguments){
+
+String favorite = "chainsaw";
+String guess = "poolcue";
+System.out.println ("Is Fin's favorite weapon a " + guess + "?");
+System.out.println("Anser: " + favorite.equals(guess));
+}
+
+}
